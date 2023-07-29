2029dc3d94b742f08d1df8cc5e2a13ae33b38bc6
winbisid <deztini4@gmail.com>
2023-07-29 17:33:35 -0400

    add text to tooltip switch

--- src/components/wheel/tooltip/TooltipSwitch.jsx
+++ src/components/wheel/tooltip/TooltipSwitch.jsx
@@ -1,29 +1,43 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
 import solidTooltip from "../../../assets/tooltip.svg";
 import lineTooltip from "../../../assets/tooltip-line.svg";
 
 export default function TooltipSwitch({ tooltips, setTooltips }) {
+  const [visibility, setVisibility] = useState("hidden");
   return (
-    <div
-      onClick={() => setTooltips((prev) => !prev)}
-      style={{
-        position: "absolute",
-        top: 0,
-        left: 0,
-        backgroundColor: tooltips ? "purple" : "indigo",
-        // backgroundColor: "indigo",
-        borderRadius: "50%",
-        margin: 20,
-      }}
-    >
-      <img
-        src={tooltips ? solidTooltip : lineTooltip}
-        width={80}
+    <div>
+      <motion.div
+        onClick={() => setTooltips((prev) => !prev)}
         style={{
-          display: "flex",
-          padding: 15,
-          //   position: "relative",
+          position: "absolute",
+          top: 0,
+          left: 0,
+          backgroundColor: tooltips ? "purple" : "indigo",
+          // backgroundColor: "indigo",
+          borderRadius: "50%",
+          margin: 20,
+          //   visibility: "hidden",
         }}
-      />
+        whileHover={() => setVisibility("visible")}
+        onHoverEnd={() => setVisibility("hidden")}
+      >
+        <img
+          src={tooltips ? solidTooltip : lineTooltip}
+          width={80}
+          style={{
+            display: "flex",
+            padding: 15,
+          }}
+        />
+      </motion.div>
+      <motion.div
+        initial={{ visibility: "hidden" }}
+        animate={{ visibility: visibility }}
+        style={{ color: "hsl(270,50%,50%)", fontSize: 25, fontWeight: 600 }}
+      >
+        tooltips {tooltips ? "on" : "off"}
+      </motion.div>
     </div>
   );
 }
