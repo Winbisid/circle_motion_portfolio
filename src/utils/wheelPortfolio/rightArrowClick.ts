6cc236173ecf0f1807c6dd9f3a9fde6069447400
winbisid <deztini4@gmail.com>
2023-08-06 23:45:06 -0400

    converted rightArrowClick to ts

--- src/utils/wheelPortfolio/rightArrowClick.ts
+++ src/utils/wheelPortfolio/rightArrowClick.ts
@@ -1,8 +1,12 @@
 // switch svg icon positions on right arrow click
-export default function rightArrowClick(iconPair, setIconPair, numOfIcons) {
+export default function rightArrowClick(
+  iconPair: { leftId: number; rightId: number },
+  setIconPair: (prev: object) => void,
+  numOfIcons: number
+) {
   if (numOfIcons) {
     if (iconPair.rightId < numOfIcons) {
-      setIconPair((prev) => ({
+      setIconPair((prev: { leftId: number; rightId: number }) => ({
         leftId: prev.leftId + 1,
         rightId: prev.rightId + 1,
       }));
