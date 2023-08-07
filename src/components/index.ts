4bdeb604f598a30aa60d52b2366b87a30ddb4ec8
winbisid <deztini4@gmail.com>
2023-08-07 01:03:10 -0400

    export & import Switch component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,4 @@ import Switch from "./switch/switchBtn";
 import Wheel from "./wheelPortfolio/Wheel";
 import Text from "./textPortfolio/Text";
 
-export { Wheel, Text };
+export { Switch, Wheel, Text };
