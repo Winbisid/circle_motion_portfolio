274cb1a0989cf668f1e66aab260d5b7b8680c167
winbisid <deztini4@gmail.com>
2023-08-17 15:45:59 -0400

    animate project to pop-up on click

--- src/components/textPortfolio/projects/Projects.tsx
+++ src/components/textPortfolio/projects/Projects.tsx
@@ -9,7 +9,8 @@ interface ProjectInterface {
 }
 
 export default function Projects() {
-  const [selectedId, setSelectedId] = useState<number | null>(null);
+  const [selectedProject, setSelectedProject] =
+    useState<ProjectInterface | null>(null);
 
   // data
   const featuredProjects: ProjectInterface[] = [
@@ -54,28 +55,86 @@ export default function Projects() {
       </div>
 
       <div
-        className="projects-container"
-        style={{ display: "flex", flexWrap: "wrap", height: "" }}
+        className="projects-wrapper"
+        onClick={() => selectedProject && setSelectedProject(null)}
+        // style={{ border: "1px solid black", height: "60%" }}
       >
-        {featuredProjects.map(({ id, name, image }) => (
-          <motion.div
-            layoutId={id}
-            onClick={() => setSelectedId(id)}
-            style={{
-              backgroundColor: "grey",
-              flex: 1,
-              margin: 10,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              // justifyContent: "center",
-              borderRadius: "1rem",
-            }}
-          >
-            <motion.img src={image} style={{ width: 120 }} />
-            <motion.h1>{name}</motion.h1>
-          </motion.div>
-        ))}
+        <div
+          className="projects-container"
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            height: "",
+            filter: selectedProject && "blur(2px)",
+            opacity: selectedProject && 0.5,
+            //   pointerEvents: selectedProject && "none",
+            //   cursor: selectedProject && "pointer",
+          }}
+        >
+          {featuredProjects.map((project) => (
+            <motion.div
+              layoutId={`${project.id}`}
+              onClick={() => setSelectedProject(project)}
+              style={{
+                backgroundColor: "grey",
+                flex: 1,
+                margin: 10,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                // justifyContent: "center",
+                borderRadius: "1rem",
+              }}
+            >
+              <motion.img src={project.image} style={{ width: 120 }} />
+              <motion.h1>{project.name}</motion.h1>
+            </motion.div>
+          ))}
+        </div>
+
+        <AnimatePresence>
+          {/* <div className="selected-project-container" style={{ width: "100%" }}> */}
+          {selectedProject && (
+            <motion.div
+              layoutId={`${selectedProject.id}`}
+              style={{
+                backgroundColor: "grey",
+                //   flex: 1,
+                margin: "0 auto",
+                // padding: 10,
+                width: "50%",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                //   justifyContent: "center",
+                borderRadius: "1rem",
+                //   position: "absolute",
+                // top:
+                // left: "50vw",
+                // right: "50vw",
+              }}
+              // initial={{
+              //   display: "flex",
+              //   flexDirection: "column",
+              //   alignItems: "center",
+              //   borderRadius: "1.2rem",
+              //   backgroundColor: "grey",
+              //   margin: "0 auto",
+              //   width: "50%",
+              // }}
+            >
+              <motion.img src={selectedProject.image} style={{ width: 250 }} />
+              <motion.h1>{selectedProject.name}</motion.h1>
+              <motion.button
+                onClick={() => setSelectedProject(null)}
+                style={{}}
+              >
+                X
+              </motion.button>
+            </motion.div>
+          )}
+          {/* </div> */}
+        </AnimatePresence>
       </div>
     </div>
   );
