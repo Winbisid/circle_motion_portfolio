ba57d05c7dd12c324829b949d4a95a39d70333fd
winbisid <deztini4@gmail.com>
2023-08-26 15:36:00 -0400

    create project-images dir & choose preferred project-card iteration

--- src/components/textPortfolio/projects/Projects.tsx
+++ src/components/textPortfolio/projects/Projects.tsx
@@ -16,63 +16,73 @@ interface CardInterface {
 }
 
 export default function Projects() {
-  const [selectedProject, setSelectedProject] =
-    useState<ProjectInterface | null>(null);
+  // const [selectedProject, setSelectedProject] =
+  //   useState<ProjectInterface | null>(null);
 
-  // temp state for secondary card
-  // const [card, setCard] = useState<CardInterface | null>(null);
-
-  // data
-  const featuredProjects: ProjectInterface[] = [
-    {
-      id: 1,
-      name: "Blerry",
-      // image: "/developer-arcade-96.png",
-      // image: "code-programing-symbol.svg",
-      image: "/work-desk-ai.jpg",
-    },
-    {
-      id: 2,
-      name: "FroGo",
-      // image: "code-programing-symbol.svg",
-      image: "/work-desk-ai.jpg",
-    },
-    {
-      id: 3,
-      name: "UpMet",
-      // image: "code-programing-symbol.svg",
-      image: "/work-desk-ai.jpg",
-    },
-    {
-      id: 4,
-      name: "ShortPort",
-      // image: "code-programing-symbol.svg",
-      image: "/work-desk-ai.jpg",
-    },
-  ];
-
-  // temp data for secondary card
+  // data for project card
   const featuredCards: CardInterface[] = [
     {
       id: 1,
-      name: "Blerry",
-      image: "/work-desk-ai.jpg",
+      name: "Angus Young",
+      image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-angus.png",
       languages: ["HTML", "CSS"],
     },
     {
       id: 2,
+      name: "Aurora",
+      image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-aurora.png",
+      languages: ["HTML", "CSS", "JavaScript", "React"],
+    },
+    {
+      id: 3,
+      name: "Visualization",
+      image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-v.png",
+      languages: ["HTML", "CSS", "TypeScript", "Next.js"],
+    },
+    {
+      id: 4,
+      name: "Visualization II",
+      image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-v2.png",
+      languages: ["TypeScript", "React Native", "Expo"],
+    },
+    {
+      id: 5,
+      name: "Visualization III",
+      image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-v3.png",
+      languages: ["HTML", "CSS"],
+    },
+    {
+      id: 6,
+      name: "Zombie",
+      image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-worms.png",
+      languages: ["HTML", "CSS", "JavaScript", "React"],
+    },
+    {
+      id: 7,
+      name: "X-Rays",
+      image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-xrays.png",
+      languages: ["HTML", "CSS", "TypeScript", "Next.js"],
+    },
+    {
+      id: 8,
+      name: "Blerry",
+      image: "/project-images/work-desk-ai.jpg",
+      languages: ["HTML", "CSS"],
+    },
+    {
+      id: 9,
       name: "FroGo",
       image: "code-programing-symbol.svg",
       languages: ["HTML", "CSS", "JavaScript", "React"],
     },
     {
-      id: 3,
+      id: 10,
       name: "UpMet",
       image: "/developer-arcade-96.png",
       languages: ["HTML", "CSS", "TypeScript", "Next.js"],
     },
     {
-      id: 4,
+      id: 11,
       name: "ShortPort",
       image: "code-programing-symbol.svg",
       languages: ["TypeScript", "React Native", "Expo"],
@@ -80,28 +90,21 @@ export default function Projects() {
   ];
 
   const [idx, setIdx] = useState<number>(0);
-  const [card, setCard] = useState<CardInterface | null>(featuredCards[idx]);
 
   function switchCardRight() {
     if (idx === featuredCards.length - 1) {
       setIdx(0);
     } else {
-      // setIdx((prev) => prev + 1);
-      setIdx(idx + 1);
+      setIdx((prev) => prev + 1);
     }
-
-    setCard(featuredCards[idx]);
   }
 
   function switchCardLeft() {
     if (idx === 0) {
       setIdx(featuredCards.length - 1);
     } else {
-      // setIdx((prev) => prev - 1);
-      setIdx(idx - 1);
+      setIdx((prev) => prev - 1);
     }
-
-    setCard(featuredCards[idx]);
   }
 
   return (
@@ -122,52 +125,9 @@ export default function Projects() {
       </div>
 
       <div className="projects-wrapper">
-        {/* <motion.div
-          className="projects-container"
-          // onClick={() => selectedProject && setSelectedProject(null)}
-          style={
-            {
-              // filter: selectedProject && "blur(2px)",
-              //   pointerEvents: selectedProject && "none",
-              // cursor: selectedProject && "pointer",
-            }
-          }
-          // animate={{ opacity: selectedProject ? 0.5 : 1 }}
-          // transition={{ duration: 1 }}
-        >
-          {featuredProjects.map((project) => (
-            <motion.div
-              className="project-card"
-              layoutId={`${project.id}`}
-              style={{
-                borderRadius: "1rem",
-              }}
-            >
-              <div className="img-div">
-                <motion.img src={project.image} />
-              </div>
-              <motion.h1>{project.name}</motion.h1>
-            </motion.div>
-          ))}
-        </motion.div> */}
-
-        {/* <div className="secondary-card">
-          <div className="img-div">
-            <img src={card?.image} alt="project image" />
-          </div>
-
-          <div className="title-div">
-            <h2>{card?.name}</h2>
-          </div>
-
-          <div className="languages-div">
-            {card?.languages.map((lang) => (
-              <p>{lang}</p>
-            ))}
-          </div>
-        </div> */}
-
-        <SecondaryCard project={card} />
+        {/* <SecondaryCard project={card} /> */}
+        <SecondaryCard project={featuredCards[idx]} />
+        {/* <CursorProjects /> */}
 
         <div>
           <button onClick={switchCardLeft}>{"<-"}</button>
@@ -238,54 +198,34 @@ export function CursorProjects() {
     },
     {
       id: 3,
-      name: "Bride of Frankenstein",
-      image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-bride.png",
+      name: "L'Homme qui rit",
+      image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-man.png",
     },
     {
       id: 4,
-      name: "Dracula",
-      image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-d.png",
+      name: "Visualization",
+      image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-v.png",
     },
-    // {
-    //   id: 1,
-    //   name: "Salvador Dalí",
-    //   image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-dali.png",
-    // },
-    // {
-    //   id: 1,
-    //   name: "Ilhuicatl Xoxouhqui",
-    //   image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-huitzi.png",
-    // },
-    // {
-    //   id: 1,
-    //   name: "L'Homme qui rit",
-    //   image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-man.png",
-    // },
-    // {
-    //   id: 1,
-    //   name: "Visualization",
-    //   image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-v.png",
-    // },
-    // {
-    //   id: 1,
-    //   name: "Visualization II",
-    //   image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-v2.png",
-    // },
-    // {
-    //   id: 1,
-    //   name: "Visualization III",
-    //   image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-v3.png",
-    // },
-    // {
-    //   id: 1,
-    //   name: "Zombie",
-    //   image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-worms.png",
-    // },
-    // {
-    //   id: 1,
-    //   name: "X-Rays",
-    //   image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-xrays.png",
-    // },
+    {
+      id: 5,
+      name: "Visualization II",
+      image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-v2.png",
+    },
+    {
+      id: 6,
+      name: "Visualization III",
+      image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-v3.png",
+    },
+    {
+      id: 7,
+      name: "Zombie",
+      image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-worms.png",
+    },
+    {
+      id: 8,
+      name: "X-Rays",
+      image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-xrays.png",
+    },
   ];
 
   const listRef = useRef();
@@ -367,17 +307,6 @@ export function CursorProjects() {
   }, [cursorXY, hoverStatus]);
 
   return (
-    // <div className="projects">
-    //   <div className="margin-wrapper">
-    //     <h1 className="heading">PROJECTS</h1>
-    //     <p className="string-text">
-    //       Venture into the labyrinth of featured projects to discover
-    //       mind-bending programming adventures. From solving intricate code
-    //       conundrums to architecting digital marvels, unravel the mysteries
-    //       behind these awe-inducing creations.
-    //     </p>
-    // </div>
-
     <div className="wrap" style={{ border: "1px solid grey" }}>
       <ul className="-center" style={{ border: "1px solid red" }}>
         {featuredProjects.map((project) => (
@@ -407,11 +336,10 @@ export function CursorProjects() {
         ))}
       </ul>
     </div>
-    // </div>
   );
 }
 
-export function SecondaryCard({ project }) {
+export function SecondaryCard({ project }: { project: CardInterface }) {
   const { name, image, languages } = project;
 
   const colors = {
@@ -427,7 +355,7 @@ export function SecondaryCard({ project }) {
 
   return (
     // <AnimatePresence>
-    <div className="secondary-card">
+    <div className="project-card">
       <div className="img-div">
         {/* <img src={image} alt={name + " project image"} /> */}
         <motion.img
