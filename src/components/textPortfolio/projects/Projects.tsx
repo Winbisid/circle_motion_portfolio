a70e602b9bdd40007a174a769a81701a098f8b0c
winbisid <deztini4@gmail.com>
2023-08-30 19:22:06 -0400

    separate featuredCards data

--- src/components/textPortfolio/projects/Projects.tsx
+++ src/components/textPortfolio/projects/Projects.tsx
@@ -7,74 +7,74 @@ export default function Projects() {
   //   useState<ProjectInterface | null>(null);
 
   // data for project card
-  const featuredCards = [
-    {
-      id: 1,
-      name: "Angus Young",
-      image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-angus.png",
-      languages: ["HTML", "CSS"],
-    },
-    {
-      id: 2,
-      name: "Aurora",
-      image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-aurora.png",
-      languages: ["HTML", "CSS", "JavaScript", "React"],
-    },
-    {
-      id: 3,
-      name: "Visualization",
-      image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-v.png",
-      languages: ["HTML", "CSS", "TypeScript", "Next.js"],
-    },
-    {
-      id: 4,
-      name: "Visualization II",
-      image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-v2.png",
-      languages: ["TypeScript", "React Native", "Expo"],
-    },
-    {
-      id: 5,
-      name: "Visualization III",
-      image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-v3.png",
-      languages: ["HTML", "CSS"],
-    },
-    {
-      id: 6,
-      name: "Zombie",
-      image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-worms.png",
-      languages: ["HTML", "CSS", "JavaScript", "React"],
-    },
-    {
-      id: 7,
-      name: "X-Rays",
-      image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-xrays.png",
-      languages: ["HTML", "CSS", "TypeScript", "Next.js"],
-    },
-    {
-      id: 8,
-      name: "Blerry",
-      image: "/project-images/work-desk-ai.jpg",
-      languages: ["HTML", "CSS"],
-    },
-    {
-      id: 9,
-      name: "FroGo",
-      image: "code-programing-symbol.svg",
-      languages: ["HTML", "CSS", "JavaScript", "React"],
-    },
-    {
-      id: 10,
-      name: "UpMet",
-      image: "/developer-arcade-96.png",
-      languages: ["HTML", "CSS", "TypeScript", "Next.js"],
-    },
-    {
-      id: 11,
-      name: "ShortPort",
-      image: "code-programing-symbol.svg",
-      languages: ["TypeScript", "React Native", "Expo"],
-    },
-  ];
+  // const featuredCards = [
+  //   {
+  //     id: 1,
+  //     name: "Angus Young",
+  //     image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-angus.png",
+  //     languages: ["HTML", "CSS"],
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Aurora",
+  //     image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-aurora.png",
+  //     languages: ["HTML", "CSS", "JavaScript", "React"],
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "Visualization",
+  //     image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-v.png",
+  //     languages: ["HTML", "CSS", "TypeScript", "Next.js"],
+  //   },
+  //   {
+  //     id: 4,
+  //     name: "Visualization II",
+  //     image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-v2.png",
+  //     languages: ["TypeScript", "React Native", "Expo"],
+  //   },
+  //   {
+  //     id: 5,
+  //     name: "Visualization III",
+  //     image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-v3.png",
+  //     languages: ["HTML", "CSS"],
+  //   },
+  //   {
+  //     id: 6,
+  //     name: "Zombie",
+  //     image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-worms.png",
+  //     languages: ["HTML", "CSS", "JavaScript", "React"],
+  //   },
+  //   {
+  //     id: 7,
+  //     name: "X-Rays",
+  //     image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/i-xrays.png",
+  //     languages: ["HTML", "CSS", "TypeScript", "Next.js"],
+  //   },
+  //   {
+  //     id: 8,
+  //     name: "Blerry",
+  //     image: "/project-images/work-desk-ai.jpg",
+  //     languages: ["HTML", "CSS"],
+  //   },
+  //   {
+  //     id: 9,
+  //     name: "FroGo",
+  //     image: "code-programing-symbol.svg",
+  //     languages: ["HTML", "CSS", "JavaScript", "React"],
+  //   },
+  //   {
+  //     id: 10,
+  //     name: "UpMet",
+  //     image: "/developer-arcade-96.png",
+  //     languages: ["HTML", "CSS", "TypeScript", "Next.js"],
+  //   },
+  //   {
+  //     id: 11,
+  //     name: "ShortPort",
+  //     image: "code-programing-symbol.svg",
+  //     languages: ["TypeScript", "React Native", "Expo"],
+  //   },
+  // ];
 
   const [idx, setIdx] = useState<number>(0);
 
