8328bfbd282d2033007856b1945c313438303790
winbisid <deztini4@gmail.com>
2023-09-01 19:29:03 -0400

    change language colors

--- src/utils/textPortfolio/colors.ts
+++ src/utils/textPortfolio/colors.ts
@@ -1,12 +1,23 @@
+// const colors = {
+//   HTML: "red",
+//   CSS: "crimson",
+//   JavaScript: "orange",
+//   TypeScript: "blue",
+//   React: "lightblue",
+//   "React Native": "skyblue",
+//   "Next.js": "grey",
+//   Expo: "aqua",
+// };
+
 const colors = {
-  HTML: "red",
-  CSS: "crimson",
-  JavaScript: "orange",
-  TypeScript: "blue",
-  React: "lightblue",
-  "React Native": "skyblue",
-  "Next.js": "grey",
-  Expo: "aqua",
+  HTML: "#E65100",
+  CSS: "#039BE5",
+  JavaScript: "#FFD600",
+  TypeScript: "#1976D2",
+  React: "#149ECA",
+  "React Native": "#80DEEA",
+  "Next.js": "#212121",
+  Expo: "#105CA0",
 };
 
 export default colors;
