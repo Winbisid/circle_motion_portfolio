61dea7ddbcf02c95d788fa24c2e1fa06dbfa3e9c
winbisid <deztini4@gmail.com>
2023-09-29 14:53:32 -0400

    add DoingNow

--- src/components/textPortfolio/Text.tsx
+++ src/components/textPortfolio/Text.tsx
@@ -4,6 +4,7 @@ import HeroBg from "./heroBg/HeroBg";
 import "./Text.css";
 import Expertise from "./expertise/Expertise";
 import Projects from "./projects/Projects";
+import DoingNow from "./Ongoing/DoingNow";
 
 export default function Text() {
   return (
