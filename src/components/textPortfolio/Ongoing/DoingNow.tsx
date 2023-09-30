8c281e5a22a3b7cb805c516c575f33d9bf4e09c5
winbisid <deztini4@gmail.com>
2023-09-30 14:30:50 -0400

    add light

--- src/components/textPortfolio/Ongoing/DoingNow.tsx
+++ src/components/textPortfolio/Ongoing/DoingNow.tsx
@@ -5,6 +5,7 @@ export default function DoingNow() {
   return (
     <div className="ongoing">
       <Canvas>
+        <ambientLight />
         <mesh>
           <sphereGeometry />
           <meshStandardMaterial color="#E1DC59" />
