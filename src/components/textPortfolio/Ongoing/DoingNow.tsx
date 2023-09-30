5fe310637c350a697d29092c505a0067233b0317
winbisid <deztini4@gmail.com>
2023-09-30 14:30:32 -0400

    add sphere geometry

--- src/components/textPortfolio/Ongoing/DoingNow.tsx
+++ src/components/textPortfolio/Ongoing/DoingNow.tsx
@@ -1,3 +1,15 @@
+import { Canvas } from "@react-three/fiber";
+import * as THREE from "three";
+
 export default function DoingNow() {
-  return <div className="ongoing">Not Last Last</div>;
+  return (
+    <div className="ongoing">
+      <Canvas>
+        <mesh>
+          <sphereGeometry />
+          <meshStandardMaterial color="#E1DC59" />
+        </mesh>
+      </Canvas>
+    </div>
+  );
 }
