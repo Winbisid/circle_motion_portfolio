acaf78d3089f4caa7ea8ca1df20a475a804e4291
winbisid <deztini4@gmail.com>
2023-10-01 14:59:16 -0400

    add planet component

--- src/components/textPortfolio/Ongoing/DoingNow.tsx
+++ src/components/textPortfolio/Ongoing/DoingNow.tsx
@@ -29,3 +29,12 @@ function Sun() {
     </mesh>
   );
 }
+
+function Planet() {
+  return (
+    <mesh position={[8, 0, 0]}>
+      <sphereGeometry args={[1, 32, 32]} />
+      <meshStandardMaterial color="#78D481" />
+    </mesh>
+  );
+}
